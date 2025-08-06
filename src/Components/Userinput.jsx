72e28642997bd8e215bdf5ba673c343d82fe7910
princemoradiya04 <princemoradiya113@gmail.com>
2025-08-06 18:53:17 +0530

    set responsive

--- src/Components/Userinput.jsx
+++ src/Components/Userinput.jsx
@@ -3,26 +3,47 @@ import React from "react";
 const Userinput = () => {
   return (
     <section>
-      <div className="flex flex-row gap-6 justify-center">
-        <p>
-          <label htmlFor="">Initial Investment:-</label>
-          <input type="number" required/>
-        </p>
-        <p>
-          <label htmlFor="">Annual Investment:-</label>
-          <input type="number" required/>
-        </p>
-      </div>
-      <div className="flex gap-6 py-10 justify-center">
-        <p>
-          <label htmlFor="">Expected Return:-</label>
-          <input type="number" required/>
-        </p>
-        <p>
-          <label htmlFor="">Duration:-</label>
-          <input type="number" required/>
-        </p>
-      </div>
+      <form  className="max-w-3xl mx-auto p-6 py-14 rounded-md shadow-md">
+        {/* First Row */}
+        <div className="flex flex-row gap-6 mb-6 justify-center">
+          <div className="flex flex-col w-full sm:w-1/2">
+            <label className="mb-1 font-medium">Initial Investment:</label>
+            <input
+              type="number"
+              required
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div className="flex flex-col w-full sm:w-1/2">
+            <label className="mb-1 font-medium">Annual Investment:</label>
+            <input
+              type="number"
+              required
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+        </div>
+
+        {/* Second Row */}
+        <div className="flex flex-row gap-6 py-10 justify-center">
+          <div className="flex flex-col w-full">
+            <label className="mb-1 font-medium">Expected Return:</label>
+            <input
+              type="number"
+              required
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div className="flex flex-col w-full ">
+            <label className="mb-1 font-medium">Duration:</label>
+            <input
+              type="number"
+              required
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+        </div>
+      </form>
     </section>
   );
 };
